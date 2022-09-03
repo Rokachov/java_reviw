9c09e5ef5cb7e75684aa7a483190566f8faf0a84
jonatahrokachov <jonatah.rokachov29@gmail.com>
2022-09-03 14:44:15 -0400

    adding some

--- src/java_review/ScannerRev.java
+++ src/java_review/ScannerRev.java
@@ -2,6 +2,14 @@ package java_review;
 
 public class ScannerRev {
 
+    public static void main(String[] args){
 
+        //1. Ask the user to enter an  integer number
+
+        //2. Ask the user to enter a decimal number
+
+        //3. Ask the user to enter a word
+
+    }
 
 }
