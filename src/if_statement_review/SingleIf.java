190b592c812d13b8d15a9c6327d88a968a583360
jonatahrokachov <jonatah.rokachov29@gmail.com>
2022-09-04 00:14:14 -0400

    if statements

---
+++ src/if_statement_review/SingleIf.java
@@ -0,0 +1,13 @@
+package if_statement_review;
+
+public class SingleIf {
+    public static void main(String[] args){
+
+        int score = 75;
+
+        if(score >= 65 ){
+            System.out.println("Congratulations on passing your exam!");
+        }
+
+    }
+}
